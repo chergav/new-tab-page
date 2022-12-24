dd4f8b124d00f5e435323e4f10869d181e4a5f6c
chergav <gav3253975@gmail.com>
2022-12-24 11:10:21 +0300

    remove vite plugin; add cp command to build

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,7 @@
 import { defineConfig } from 'vite';
-//import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-	plugins: [
-		// viteStaticCopy({
-		// 	targets: [
-		// 		{
-		// 			src: '_locales',
-		// 			dest: '',
-		// 		},
-		// 		{
-		// 			src: 'manifest.json',
-		// 			dest: '',
-		// 		},
-		// 		{
-		// 			src: 'icons',
-		// 			dest: '',
-		// 		},
-		// 	],
-		// }),
-	],
+	plugins: [],
 	server: {
 		port: 3000,
 	},
